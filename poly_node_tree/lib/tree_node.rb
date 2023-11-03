a06ee385c2d76154bfcfc34a203814ea397214e0
Mujahed Ali <mujahed97@gmail.com>
2023-11-03 15:32:32 -0400

    finished part1

--- poly_node_tree/lib/tree_node.rb
+++ poly_node_tree/lib/tree_node.rb
@@ -1,3 +1,39 @@
 class PolyTreeNode
+
+    attr_reader :value, :parent, :children
   
+    def initialize(root)
+        @value = root
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(newParent)  
+        if self.parent != nil
+            self.parent.children.delete(self)
+        end
+        @parent = newParent
+        newParent.children << self if newParent != nil
+    end
+   
+    def add_child(child)
+        child.parent = self
+        if !self.children.include?(child)
+            self.children << child
+        end
+    end
+
+    def remove_child(child)
+        if child.parent == nil
+            raise "Child has no parent"
+        end
+        child.parent = nil
+    end
+
+    # def remove_child
+    # end
+
+
+
+
 end
